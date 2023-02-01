34e071b0788bf965730ff812d83a0365c805e7db
Martin Todev <mtodev@uwo.ca>
2023-01-31 20:45:50 -0500

    Created model

---
+++ backend-express/Authentication/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+});
+
+const User = mongoose.model('user');
