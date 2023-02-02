6e01fca795f1527a67b1d1aa786d564a007d2a45
gaugust2 <gaugust2@uwo.ca>
2023-02-02 18:14:00 -0500

    searchbar for PDF file names

--- outline-app/src/Mainpage.jsx
+++ outline-app/src/Mainpage.jsx
@@ -3,26 +3,54 @@ import axios from 'axios';
 import { saveAs } from 'file-saver';
 
 const MainPage = () => {
+  const [pdfFiles, setPdfFiles] = useState([]);
+  const [selectedFile, setSelectedFile] = useState(null);
   const [pdfBlob, setPdfBlob] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
 
-  const getPDF = async () => {
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get('/fetch-pdf-files');
+        setPdfFiles(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
+  const getPDF = async (file) => {
     try {
-      const res = await axios.get('/fetch-pdf', { responseType: 'blob' });
+      const res = await axios.get(`/fetch-pdf/${file.id}`, { responseType: 'blob' });
       setPdfBlob(new Blob([res.data], { type: 'application/pdf' }));
+      setSelectedFile(file);
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredFiles = pdfFiles.filter(file => file.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
   return (
     <div>
-      <button onClick={getPDF}>View PDF</button>
-      {pdfBlob && (
+      <input type="text" placeholder="Search for PDF" value={searchTerm} onChange={handleSearch} />
+      <ul>
+        {filteredFiles.map(file => (
+          <li key={file.id}>
+            <button onClick={() => getPDF(file)}>{file.name}</button>
+          </li>
+        ))}
+      </ul>
+      {selectedFile && (
         <a
           href={URL.createObjectURL(pdfBlob)}
           target="_blank"
           rel="noopener noreferrer"
-          download="CourseOutline.pdf"
+          download={`${selectedFile.name}.pdf`}
         >
           Download PDF
         </a>
@@ -32,7 +60,3 @@ const MainPage = () => {
 };
 
 export default MainPage;
-
-
-
-
